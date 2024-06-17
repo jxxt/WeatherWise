2338605a59d68a50d03a8a6be25cb525b703bb7c
Jeet Debnath <jeet.debnath2004@gmail.com>
2024-06-17 17:34:17 +0530

    improved dom

--- script.js
+++ script.js
@@ -7,30 +7,14 @@ const submitBtn = document.querySelector(".submit");
 
 let API_KEY;
 
-if (localStorage.getItem("API_KEY") !== null) {
-    API_KEY = localStorage.getItem("API_KEY");
+const weatherReport = async (API_KEY, locationn) => {
+    const weatherDiv = document.querySelector(".weather");
 
-    apiKeyInput.classList.add("hide");
+    selection.classList.add("hide");
+    weatherDiv.classList.remove("hide");
 
-    submitBtn.addEventListener("click", () => {
-        let location = locationInput.value;
-        console.log(API_KEY);
-        console.log(location);
-    });
-} else {
-    submitBtn.addEventListener("click", () => {
-        const API_KEY = apiKeyInput.value;
-        let location = locationInput.value;
-        console.log(API_KEY);
-        console.log(location);
-
-        localStorage.setItem("API_KEY", API_KEY);
-    });
-}
-
-const weatherReport = async (location) => {
     const response = await fetch(
-        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}`,
+        `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${locationn}`,
         { mode: "cors" }
     );
 
@@ -40,43 +24,48 @@ const weatherReport = async (location) => {
     const regionName = responseJson.location.region;
     const countryName = responseJson.location.country;
 
-    // console.log(countryName);
-
     const icon = responseJson.current.condition.icon;
     const text = responseJson.current.condition.text;
-    const temp = responseJson.current.temp_c;
-
-    const weatherDiv = document.querySelector(".weather");
+    const temp = Math.round(responseJson.current.temp_c);
 
     const locationElement = document.createElement("div");
-    locationElement.classList.add("location-name");
-    locationElement.innerHTML = `<div class="location-name">${locationName}</div>`;
+    locationElement.classList.add("location-details");
+    locationElement.innerHTML = `<div class="line1"><div class="location-name">${locationName}</div></div><div class="line2"><div class="region-name">${regionName}, ${countryName}</div></div>`;
     weatherDiv.appendChild(locationElement);
 
-    const regionElement = document.createElement("div");
-    regionElement.classList.add("region-name");
-    regionElement.innerHTML = `<div class="region-name">${regionName}</div>`;
-    weatherDiv.appendChild(regionElement);
-
-    const countryElement = document.createElement("div");
-    countryElement.classList.add("country-name");
-    countryElement.innerHTML = `<div class="country-name">${countryName}</div>`;
-    weatherDiv.appendChild(countryElement);
-
     const iconElement = document.createElement("div");
-    iconElement.classList.add("icon-div");
-    iconElement.innerHTML = `<img src="https:${icon}" alt="" class="icon">`;
+    iconElement.classList.add("icon-temp");
+    iconElement.innerHTML = `<img src="https:${icon}" alt="" class="icon"><div class="temp">${temp}°C</div>`;
     weatherDiv.appendChild(iconElement);
 
     const textElement = document.createElement("div");
     textElement.classList.add("text");
     textElement.innerHTML = `<div class="country-name">${text}</div>`;
     weatherDiv.appendChild(textElement);
-
-    const tempElement = document.createElement("div");
-    tempElement.classList.add("temp");
-    tempElement.innerHTML = `<div class="country-name">${temp}</div>`;
-    weatherDiv.appendChild(tempElement);
 };
 
-weatherReport("mumbai");
+if (
+    localStorage.getItem("API_KEY") !== null &&
+    localStorage.getItem("location") !== null
+) {
+    API_KEY = localStorage.getItem("API_KEY");
+    let locationn = localStorage.getItem("location");
+
+    apiKeyInput.classList.add("hide");
+    locationInput.classList.add("hide");
+
+    weatherReport(API_KEY, locationn);
+} else {
+    submitBtn.addEventListener("click", () => {
+        API_KEY = apiKeyInput.value;
+        let locationn = locationInput.value;
+
+        console.log(API_KEY);
+        console.log(location);
+
+        localStorage.setItem("API_KEY", API_KEY);
+        localStorage.setItem("location", locationn);
+
+        weatherReport(API_KEY, locationn);
+    });
+}
